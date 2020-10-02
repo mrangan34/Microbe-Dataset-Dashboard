7d02515e18d96c8bd449b4ea4d59bfd00111fd5c
Mallika Rangan <mallikarangan@Mallikas-MBP.home>
2020-10-01 22:31:52 -0400

    app fetches corresponding sample

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,24 +1,22 @@
 //Read samples.json
 d3.json("samples.json").then(function(bellybutton) {
-    //console.log(bellybutton.samples);
+    console.log(bellybutton.samples);
+
+    //creating arrays from larger array
     var bellybutton_array = bellybutton.samples;
-    console.log("printing bellybutton array")
-    console.log(bellybutton_array);
+    
+    // console.log("printing bellybutton array")
+    // console.log(bellybutton_array);
 
     // var bellybutton_map = bellybutton_array.map(object=>object.id);
     // console.log(bellybutton_map);
-
-
-
-    var bellybutton_array = bellybutton.samples;
 
     var sample_ids_map = bellybutton_array.map(object=>object.id);
     // console.log("printing sample ID map");
     // console.log(sample_ids_map);
 
-
     var otu_ids_map = bellybutton_array.map(object=>object.otu_ids);
-    console.log(otu_ids_map);
+    // console.log(otu_ids_map);
 
     var sample_values_map = bellybutton_array.map(object=>object.sample_values);
     // console.log(sample_values_map);
@@ -26,7 +24,7 @@ d3.json("samples.json").then(function(bellybutton) {
     var otu_labels_map = bellybutton_array.map(object=>object.otu_labels);
     // console.log(otu_labels_map);
 
-    // Second, create new elements for extra data points
+    // add every sample to the dropdown menu
     d3.select("select")
     .selectAll("option")
     .data(bellybutton_array)
@@ -36,71 +34,76 @@ d3.json("samples.json").then(function(bellybutton) {
     return `${d.id}`;
     });
 
-//     // Create an array of each country's numbers
-// var us = Object.values(data.us);
-// var uk = Object.values(data.uk);
-// var canada = Object.values(data.canada);
+    var defaultData = bellybutton_array[0].id;
+    console.log("now loading graphs for sample number")
+    console.log(defaultData);
+    
 
-// // Create an array of music provider labels
-// var labels = Object.keys(data.us);
+    // var data = [{
+    //     type: 'bar',
+    //     x: [20, 14, 23],
+    //     y: ['giraffes', 'orangutans', 'monkeys'],
+    //     orientation: 'h'
+    //   }];
+      
+    //   Plotly.newPlot('bar', data);
 
-// Display the default plot
-// function init() {
-//   var data = [{
-//     values: us,
-//     labels: labels,
-//     type: "pie"
-//   }];
+    //   Plotly.newPlot('bubble', data, layout); 
+    // // Bar Chart
+    // var trace1 = {
+    //     x: sample_values.slice(0,10).reverse(),
+    //     y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+    //     text: otu_labels.slice(0,10).reverse(),
+    //     name: "Greek",
+    //     type: "bar",
+    //     orientation: "h"
+    // };
+    // var data = [trace1];
+    // var layout = {
+    //     title: "Top Ten OTUs for Individual " +sample,
+    //     margin: {l: 100, r: 100, t: 100, b: 100}
+    // };
+    // Plotly.newPlot("bar", data, layout);  
+    // });
+  
 
-//   var layout = {
-//     height: 600,
-//     width: 800
-//   };
+    // On change to the DOM, call getData()
+    d3.selectAll("#selDataset").on("change", getData);
 
-//   Plotly.newPlot("pie", data, layout);
-// }
+    // Function called by DOM changes
+    function getData() {
+        console.log("a change in selection has occured");
+        var dropdownMenu = d3.select("#selDataset");
+    //     // Assign the value of the dropdown menu option to a variable
+        var dataset = dropdownMenu.property("value");
+        console.log(dataset)
+    //     // Initialize an empty array for the sample's data
+    //     // var data = [];
+        var newDataSample = bellybutton_array.filter(findSample => findSample.id == dataset);
+            console.log("printing chosen sample");
+            console.log(newDataSample);
+    }
+    //  
+        
+        // }
+       
 
-// On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change", getData);
+    // );
+    
 
-// Function called by DOM changes
-function getData() {
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    // Initialize an empty array for the sample's data
-    var data = [];
-    var result = bellybutton_array.find(function(e){
-        return e.id == dataset;
+    // //set the data for the default graphs to be the first sample
 
 
-  })
-  console.log("the result is:")
-  console.log(result)
+    // //change the chart data if the dropdown menu selection changes
+    // var newData = bellybutton_array.filter(object => object.id);
+    // var filteredData = samples.filter(sampleObject => sampleObject.id == sample);
 
-    // if (dataset == '944') {
 
-    // var result = bellybutton_array.find(function(e) {
-    //     return e.id == '944';
-    //     });
-    //     console.log("the resulting array object is:")
-    //     console.log(result)
 
-//   }
-//   else if (dataset == '950') {
-//       data = uk;
-//   }
-//   else if (dataset == '958') {
-//       data = canada;
-//   }
-  // Call function to update the chart
-  updatePlotly(data);
-}
 
-// Update the restyled plot's values
-function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
-}
+
+
+
 
 init();
 
